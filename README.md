d66bd98c41c63a57ccff94939c9856bc88a7fc27
emipa606 <emil.offermo@litet.net>
2021-09-04 21:02:45 +0200

    New version of 1SettingsHelper

---
+++ README.md
@@ -0,0 +1,70 @@
+# ClothingSorter
+
+![Image](https://i.imgur.com/buuPQel.png)
+
+
+## A mod that sorts apparel based on various properties
+
+
+
+
+- Option to sort by layer
+- Option to sort by tech-level
+- Option to sort by mod-name
+
+
+
+
+- Option to combine layer-categories with the same name
+- Options for subcategories based on armor-value and Psycic/Royalty-tagged apparel
+- Option to set what armor-value apparel should be put in the armored category
+- A CE-multiplier option to fine tune the CE-armor calculation
+
+
+
+
+- Works with all apparel, modded and vanilla
+- Generates categories for custom apparel-layers
+- Safe to add/remove from saves
+- Verified to work with CE
+
+
+
+Russian translation by Reiquard
+Chinese translation by HawnHan
+French translation by qux
+Korean translation by isty2e
+
+Keywords: Sort, filter, apparel, categories, layers
+
+[table]
+	[tr]
+		[td]https://invite.gg/Mlie]![Image](https://i.imgur.com/zdzzBrc.png)
+[/td]
+		[td]https://github.com/emipa606/ClothingSorter]![Image](https://i.imgur.com/kTkpTOE.png)
+[/td]
+	[/tr]
+[/table]
+
+If you have any suggestion on more properties to sort on, leave a comment here or on the Discord-server!
+
+![Image](https://i.imgur.com/O0IIlYj.png)
+
+Since modding is just a hobby for me I expect no donations to keep modding. If you still want to show your support you can gift me anything from my https://store.steampowered.com/wishlist/id/Mlie]Wishlist or buy me a cup of tea.
+
+https://ko-fi.com/G2G55DDYD]![Image](https://i.ibb.co/VWJJb3w/Support-Me-dark-2x.png)
+
+
+![Image](https://i.imgur.com/PwoNOj4.png)
+
+
+
+-  See if the the error persists if you just have this mod and its requirements active.
+-  If not, try adding your other mods until it happens again.
+-  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib and command Ctrl+F12
+-  For best support, please use the Discord-channel for error-reporting.
+-  Do not report errors by making a discussion-thread, I get no notification of that.
+-  If you have the solution for a problem, please post it to the GitHub repository.
+
+
+
---
+++ Source/ClothingSorter/Settings/Mod.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using SettingsHelper;
+using UnityEngine;
+using Verse;
+
+namespace ClothingSorter
+{
+    [StaticConstructorOnStartup]
+    internal class ClothingSorterMod : Mod
+    {
+        /// <summary>
+        ///     The instance of the settings to be read by the mod
+        /// </summary>
+        public static ClothingSorterMod instance;
+
+        /// <summary>
+        ///     The private settings
+        /// </summary>
+        private ClothingSorterSettings settings;
+
+        /// <summary>
+        ///     Cunstructor
+        /// </summary>
+        /// <param name="content"></param>
+        public ClothingSorterMod(ModContentPack content) : base(content)
+        {
+            instance = this;
+        }
+
+        /// <summary>
+        ///     The instance-settings for the mod
+        /// </summary>
+        internal ClothingSorterSettings Settings
+        {
+            get
+            {
+                if (settings == null)
+                {
+                    settings = GetSettings<ClothingSorterSettings>();
+                }
+
+                return settings;
+            }
+            set => settings = value;
+        }
+
+        /// <summary>
+        ///     The title for the mod-settings
+        /// </summary>
+        /// <returns></returns>
+        public override string SettingsCategory()
+        {
+            return "Clothing Sorter";
+        }
+
+        /// <summary>
+        ///     The settings-window
+        ///     For more info: https://rimworldwiki.com/wiki/Modding_Tutorials/ModSettings
+        /// </summary>
+        /// <param name="rect"></param>
+        public override void DoSettingsWindowContents(Rect rect)
+        {
+            var listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(rect);
+            GUI.contentColor = Color.yellow;
+            listing_Standard.Label("CS_SettingDeselectOptions".Translate());
+            GUI.contentColor = Color.white;
+            if (!(Settings.SortByLayer || Settings.SortByTech || Settings.SortByMod))
+            {
+                Settings.SortByLayer = true;
+            }
+
+            if (AtLeastTwo(Settings.SortByLayer, Settings.SortByTech, Settings.SortByMod))
+            {
+                var categories = new string[2];
+                if (Settings.SortByLayer)
+                {
+                    listing_Standard.CheckboxLabeled("CS_SettingLayerCategories".Translate(), ref Settings.SortByLayer,
+                        "CS_SettingLayerCategoriesDescription".Translate());
+                    categories[0] = "CS_SettingLayer".Translate();
+                }
+                else
+                {
+                    GUI.contentColor = Color.grey;
+                    listing_Standard.Label("CS_SettingLayerCategories".Translate(), -1,
+                        "CS_SettingDeselectOptions".Translate());
+                    GUI.contentColor = Color.white;
+                }
+
+                if (Settings.SortByTech)
+                {
+                    listing_Standard.CheckboxLabeled("CS_SettingTechCategories".Translate(), ref Settings.SortByTech,
+                        "CS_SettingTechCategoriesDescription".Translate());
+                    if (string.IsNullOrEmpty(categories[0]))
+                    {
+                        categories[0] = "CS_SettingTech".Translate();
+                    }
+                    else
+                    {
+                        categories[1] = "CS_SettingTech".Translate();
+                    }
+                }
+                else
+                {
+                    GUI.contentColor = Color.grey;
+                    listing_Standard.Label("CS_SettingTechCategories".Translate(), -1,
+                        "CS_SettingDeselectOptions".Translate());
+                    GUI.contentColor = Color.white;
+                }
+
+                if (Settings.SortByMod)
+                {
+                    listing_Standard.CheckboxLabeled("CS_SettingModCategories".Translate(), ref Settings.SortByMod,
+                        "CS_SettingModCategoriesDescription".Translate());
+                    categories[1] = "CS_SettingMod".Translate();
+                }
+                else
+                {
+                    GUI.contentColor = Color.grey;
+                    listing_Standard.Label("CS_SettingModCategories".Translate(), -1,
+                        "CS_SettingDeselectOptions".Translate());
+                    GUI.contentColor = Color.white;
+                }
+
+                listing_Standard.Gap();
+                listing_Standard.Label("CS_SettingSortOrder".Translate());
+                if (listing_Standard.RadioButton($"{categories[0]} / {categories[1]}",
+                    Settings.SortSetting == 0))
+                {
+                    Settings.SortSetting = 0;
+                }
+
+                if (listing_Standard.RadioButton($"{categories[1]} / {categories[0]}",
+                    Settings.SortSetting == 1))
+                {
+                    Settings.SortSetting = 1;
+                }
+            }
+            else
+            {
+                listing_Standard.CheckboxLabeled("CS_SettingLayerCategories".Translate(), ref Settings.SortByLayer,
+                    "CS_SettingLayerCategoriesDescription".Translate());
+                listing_Standard.CheckboxLabeled("CS_SettingTechCategories".Translate(), ref Settings.SortByTech,
+                    "CS_SettingTechCategoriesDescription".Translate());
+                listing_Standard.CheckboxLabeled("CS_SettingModCategories".Translate(), ref Settings.SortByMod,
+                    "CS_SettingModCategoriesDescription".Translate());
+
+                GUI.contentColor = Color.grey;
+                listing_Standard.Gap();
+                listing_Standard.Label("CS_SettingSortOrder".Translate());
+                listing_Standard.Label("/");
+                listing_Standard.Label("/");
+                GUI.contentColor = Color.white;
+            }
+
+            listing_Standard.GapLine();
+            if (Settings.SortByLayer)
+            {
+                listing_Standard.CheckboxLabeled("CS_SettingCombineLayers".Translate(), ref Settings.CombineLayers,
+                    "CS_SettingCombineLayersDescription".Translate());
+            }
+            else
+            {
+                GUI.contentColor = Color.grey;
+                listing_Standard.Label("CS_SettingCombineLayers".Translate(), -1,
+                    "CS_SettingCombineLayersDescription".Translate());
+                GUI.contentColor = Color.white;
+            }
+
+            listing_Standard.CheckboxLabeled("CS_SettingArmoredCategories".Translate(), ref Settings.ArmoredSeparate,
+                "CS_SettingArmoredCategoriesDescription".Translate());
+            if (Settings.ArmoredSeparate)
+            {
+                listing_Standard.AddLabeledSlider(
+                    "CS_SettingArmoredLowValue".Translate(Math.Round(Settings.ArmorRating * 100)),
+                    ref Settings.ArmorRating, 0f, 2f, "CS_SettingArmoredMin".Translate(),
+                    "CS_SettingArmoredMax".Translate(), 0.01f);
+                listing_Standard.Gap();
+                if (ModLister.GetActiveModWithIdentifier("CETeam.CombatExtended") != null)
+                {
+                    listing_Standard.Label("CS_SettingCEDescription".Translate());
+                    listing_Standard.AddLabeledSlider("CS_SettingCEArmorModifier".Translate(Settings.CEArmorModifier),
+                        ref Settings.CEArmorModifier, 0f, 10f, null, null, 0.1f);
+                    listing_Standard.Gap();
+                }
+
+                listing_Standard.GapLine();
+            }
+
+            if (ModLister.RoyaltyInstalled)
+            {
+                listing_Standard.CheckboxLabeled("CS_SettingPsyfocusCategories".Translate(),
+                    ref Settings.PsychicSeparate, "CS_SettingPsyfocusCategoriesDescription".Translate());
+                listing_Standard.CheckboxLabeled("CS_SettingRoyaltyCategories".Translate(),
+                    ref Settings.RoyaltySeparate, "CS_SettingRoyaltyCategoriesDescription".Translate());
+            }
+
+            listing_Standard.End();
+
+            Settings.Write();
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            ClothingSorter.SortClothing();
+        }
+
+        public static bool AtLeastTwo(List<bool> listOfBool)
+        {
+            if (listOfBool.Count != 3)
+            {
+                return false;
+            }
+
+            return AtLeastTwo(listOfBool[0], listOfBool[1], listOfBool[2]);
+        }
+
+        private static bool AtLeastTwo(bool a, bool b, bool c)
+        {
+            return a && (b || c) || b && c;
+        }
+    }
+}
